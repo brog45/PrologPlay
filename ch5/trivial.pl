852f8355a617c27ad15a4b028b84713b7e4d4674
Brian Rogers <brog45@gmail.com>
2019-02-12 00:05:06 -0600

    a trivial OOPS example for simple testing

---
+++ ch5/trivial.pl
@@ -0,0 +1,18 @@
+% trivial.pl: a trivial OOPS example for simple testing
+% This trivial script simply prints "Hello, Prolog!"
+
+% load oops operator definitions
+:- consult(oops_ops).
+
+% initial_data/1 will be used to initialize working storage
+initial_data([
+        hello
+    ]).
+
+rule 1#
+    [ 1# hello ]
+    ==>
+    [ write("Hello, Prolog!")
+    , nl
+    , retract(1) % removes hello from working storage
+    ].
